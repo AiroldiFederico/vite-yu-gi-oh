c4cf5b341d28c85ca56b80156bae230f51f4867c
Federico Airoldi <airoldibusiness@gmail.com>
2023-05-06 20:26:04 +0200

    FASE 1 - preparo il contenitore delle carte

--- src/components/ListCardsComp.vue
+++ src/components/ListCardsComp.vue
@@ -29,6 +29,14 @@
 <template>
     <div id="ListCardsComp">
 
+        <div id="TopBar">
+            <p>Found n/ cards</p>
+        </div>
+
+        <div id="CardContainer">
+
+        </div>
+
     </div>
 </template>
 
@@ -42,9 +50,22 @@
 #ListCardsComp {
 
     color: white;
-    width: 10rem;
-    height: 10rem;
+    width: 80rem;
+    //height: 40rem;
     background-color: white;
+    padding: 1rem;
+
+    #TopBar {
+        background-color: black;
+        line-height: 2.5rem;
+        text-indent: 1rem;
+    }
+
+    #CardContainer {
+        background-color: rgb(212, 255, 255);
+        width: 100%;
+        height: 40rem;
+    }
 
 }
 
